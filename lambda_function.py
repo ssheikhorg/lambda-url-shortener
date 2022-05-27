2380f6489395761a037bfdb2219fc18bb76a6a5f
Shapon Sheikh <ssheikhorg@hotmail.com>
2022-05-27 09:33:29 +0600

    worked on dynamodb table and data

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,8 @@
 import json
 import string
+from datetime import datetime
 from random import choices
-
+import uuid
 import boto3
 
 
@@ -13,6 +14,20 @@ def lambda_handler(event, context):
     short_url = generate_short_id(event['url'])
     print("short_url:--", short_url)
     
+    date_time = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+
+    dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+    table = dynamodb.Table('url_shortener')
+
+    table.put_item(Item={
+        'id': str(uuid.uuid4()),
+        'createdDate':date_time,
+        'originalUrl': event['frequency'],
+        'shortUrl': event['orderAmount']
+    })
+
+
+
     return {
         'statusCode': 200,
         # 'body': json.dumps(f'Hello from Lambda! {short_url}'),
